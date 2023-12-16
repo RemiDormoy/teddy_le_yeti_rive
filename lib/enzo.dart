249531e0bdb990b39b08b8e11715e4b75367b550
remi.dormoy <remi.dormoy@accenture.com>
2023-12-16 17:34:53 +0100

    Point 0 sans rive

--- lib/enzo.dart
+++ lib/enzo.dart
@@ -1,78 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:rive/rive.dart';
 
-class EnzoAuraitDuFaireCaPage extends StatefulWidget {
+class LoginPage extends StatefulWidget {
   @override
-  State<EnzoAuraitDuFaireCaPage> createState() => _EnzoAuraitDuFaireCaPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
-class _EnzoAuraitDuFaireCaPageState extends State<EnzoAuraitDuFaireCaPage> {
+class _LoginPageState extends State<LoginPage> {
   final titleStyle = const TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
-  late RiveAnimationController successAnimation;
-  late RiveAnimationController failAnimation;
-  late StateMachineController _stateController;
   bool isLoading = false;
   bool isError = false;
   bool isSuccess = false;
-  late SMIInput<bool> handsUpController;
-  late SMIInput<bool> failController;
-  late SMIInput<bool> successController;
-  late SMIInput<double> oeilController;
-  late SMIInput<bool> checkingController;
 
   final mdpController = TextEditingController();
   final loginController = TextEditingController();
-  FocusNode _focus = FocusNode();
-  FocusNode _focusLogin = FocusNode();
-
-  @override
-  void initState() {
-    super.initState();
-    _focus.addListener(_onFocusChange);
-    _focusLogin.addListener(_onFocusChangeLogin);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _focus.removeListener(_onFocusChange);
-    _focusLogin.removeListener(_onFocusChangeLogin);
-    _focus.dispose();
-  }
-
-  void _onFocusChange() {
-    if (_focus.hasFocus) {
-      handsUpController.value = true;
-    } else {
-      handsUpController.value = false;
-    }
-  }
-
-  void _onFocusChangeLogin() {
-    if (_focusLogin.hasFocus) {
-      print('bonjour bonjour');
-      checkingController.value = true;
-    } else {
-      print('aurevoir aurevoir');
-      checkingController.value = false;
-    }
-  }
-
-  void _onInit(Artboard art) {
-    var ctrl = StateMachineController.fromArtboard(art, 'Login Machine') as StateMachineController;
-    ctrl.isActive = false;
-    art.addController(ctrl);
-    setState(() {
-      _stateController = ctrl;
-    });
-    handsUpController = _stateController.findInput<bool>('isHandsUp')!;
-    failController = _stateController.findInput<bool>('trigFail')!;
-    successController = _stateController.findInput<bool>('trigSuccess')!;
-    oeilController = _stateController.findInput<double>('numLook')!;
-    checkingController = _stateController.findInput<bool>('isChecking')!;
-    failController.value = true;
-    successController.value = true;
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -94,15 +34,6 @@ class _EnzoAuraitDuFaireCaPageState extends State<EnzoAuraitDuFaireCaPage> {
                       child: Container(
                         color: Colors.black,
                         height: 300,
-                        child: RiveAnimation.asset(
-                          'assets/teddy.riv',
-                          onInit: _onInit,
-                          fit: BoxFit.fitHeight,
-                          animations: const [
-                            'idle'
-                          ], //, 'Hands_up', 'hands_down', 'success', 'fail', 'Look_down_right', 'Look_down_left', 'look_idle'],
-                          //controllers: [successAnimation, failAnimation],
-                        ),
                       ),
                     ),
                     Padding(
@@ -116,7 +47,6 @@ class _EnzoAuraitDuFaireCaPageState extends State<EnzoAuraitDuFaireCaPage> {
                           children: [
                             Text('Identifiant', style: titleStyle),
                             TextField(
-                              focusNode: _focusLogin,
                               decoration: const InputDecoration(
                                 border: UnderlineInputBorder(),
                                 hintText: 'Mail ou identifiant',
@@ -128,14 +58,12 @@ class _EnzoAuraitDuFaireCaPageState extends State<EnzoAuraitDuFaireCaPage> {
                                     isError = false;
                                   });
                                 }
-                                oeilController.value = value.length * 5;
                               },
                               controller: loginController,
                             ),
                             const SizedBox(height: 24),
                             Text('Mot de passe', style: titleStyle),
                             TextField(
-                              focusNode: _focus,
                               obscureText: true,
                               enableSuggestions: false,
                               autocorrect: false,
@@ -184,18 +112,14 @@ class _EnzoAuraitDuFaireCaPageState extends State<EnzoAuraitDuFaireCaPage> {
                                         isError = false;
                                       });
                                       FocusScope.of(context).unfocus();
-                                      failController.value = false;
-                                      successController.value = false;
                                       Future.delayed(const Duration(seconds: 2)).then((value) {
                                         if (mdpController.text == 'yoloyolo' && loginController.text == 'remileboss') {
-                                          successController.value = true;
                                           setState(() {
                                             isLoading = false;
                                             isError = false;
                                             isSuccess = true;
                                           });
                                         } else {
-                                          failController.value = true;
                                           setState(() {
                                             isLoading = false;
                                             isError = true;
